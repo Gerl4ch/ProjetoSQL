e5e60d93a79204eecfbe477ee0f0cfa7325f4d25
Gerl4ch <leonardohg2005@gmail.com>
2024-08-02 19:51:18 -0300

    Script

--- dbPets.sql
+++ dbPets.sql
@@ -1,4 +1,4 @@
---Apagar o BD
+-- Apagar o BD
 
 drop database dbPets;
 
@@ -14,3 +14,25 @@ use dbPets;
 
 show databases;
 
+-- Criando tabelas no BD
+
+create table tbUsuarios(
+codUsu int not null AUTO_INCREMENT,
+nome varchar(50) not null,
+senha varchar(50) not null,
+primary key(codUsu)
+);
+
+create table tbFuncionarios(
+codFunc int not null AUTO_INCREMENT,
+nome varchar(100) not null,
+email varchar(100),
+telCel char(10),
+);
+-- Visualizando tabelas
+
+show tables;
+
+-- Visualizando a estrutura 
+
+desc tbUsuarios;
